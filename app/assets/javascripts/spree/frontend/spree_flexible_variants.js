f241ed2a1a151475ef776e9686a52eb26f29cd35
Craig Lomax <craig@clomax.me.uk>
2015-02-28 13:39:49 +0000

    Spinners and radios report correct values.

--- app/assets/javascripts/spree/frontend/spree_flexible_variants.js
+++ app/assets/javascripts/spree/frontend/spree_flexible_variants.js
@@ -1,2 +1,41 @@
-// Placeholder manifest file.
-// the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/frontend/all.js'
+(function() {
+  $(function() {
+    var number_of_units, quantity, quantity_spinner, radios, selectedRadio, unit_amount;
+
+    unit_amount = 0;
+    quantity = 0;
+
+    Spree.updateTotalUnits = function(variant) {
+      return unit_amount = variant.data('unitAmount');
+    };
+
+    Spree.updateQuantity = function(q_spinner) {
+      return quantity = q_spinner[0].valueAsNumber;
+    };
+
+    Spree.checkStock = function () {
+      return quantity * unit_amount;
+    };
+
+    radios = $('#product-variants input[type="radio"]');
+    if (radios.length > 0) {
+      selectedRadio = $('#product-variants input[type="radio"][checked="checked"]');
+      Spree.updateTotalUnits(selectedRadio);
+    }
+
+    var number_of_units = ($('span#number_of_units')).data('num-units');
+    var quantity_spinner = $('input#quantity[type="number"]');
+    var spinner = quantity_spinner.spinner({
+      change: function (event, ui) {
+        Spree.updateQuantity($(this));
+      },
+    });
+
+    return radios.click(function(event) {
+      Spree.updateTotalUnits($(this));
+      Spree.updateQuantity(quantity_spinner);
+    });
+  });
+
+}).call(this);
+
--- app/assets/javascripts/spree/frontend/spree_flexible_variants.js.coffee
+++
@@ -1,7 +0,0 @@
-$ ->
-  Spree.updateTotalUnits = (variant) ->
-    unit_amount = variant.data('unit_amount')
-    console.log(unit_amount)
-
-  radios.click (event) ->
-    Spree.updateTotalUnits ($ this)
