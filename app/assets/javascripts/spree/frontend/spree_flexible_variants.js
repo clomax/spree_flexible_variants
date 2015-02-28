880f565dedc5a2f279cd19d74b70f03e8a7ee98b
Craig Lomax <craig@clomax.me.uk>
2015-02-28 17:10:01 +0000

    Only check for products with flexible stock.

--- app/assets/javascripts/spree/frontend/spree_flexible_variants.js
+++ app/assets/javascripts/spree/frontend/spree_flexible_variants.js
@@ -18,7 +18,7 @@
       var button = document.getElementById('add-to-cart-button');
       if (inStock == true) {
         button.disabled = false;
-        button.style.background="#000000";
+        button.style.background="#808080";
         button.textContent = "Add To Cart";
       } else {
         button.disabled = true;
@@ -28,29 +28,30 @@
       return inStock;
     };
 
-    radios = $('#product-variants input[type="radio"]');
-    if (radios.length > 0) {
-      selectedRadio = $('#product-variants input[type="radio"][checked="checked"]');
-      Spree.updateTotalUnits(selectedRadio.data('unitAmount'));
-    }
 
     var number_of_units = ($('span#number_of_units')).data('num-units');
-    var quantity_spinner = document.getElementById('quantity');
-console.log(unit_amount);
-    Spree.updateTotalUnits(unit_amount);
-    Spree.updateQuantity(quantity);
-    Spree.checkStock();
+      if (number_of_units) {
+        radios = $('#product-variants input[type="radio"]');
+        if (radios.length > 0) {
+          selectedRadio = $('#product-variants input[type="radio"][checked="checked"]');
+          Spree.updateTotalUnits(selectedRadio.data('unitAmount'));
+        }
 
-    quantity_spinner.addEventListener("input", function(e) {
-      Spree.updateQuantity(quantity_spinner.valueAsNumber);
-      return console.log(Spree.checkStock());
-    })
+        var quantity_spinner = document.getElementById('quantity');
+        Spree.updateTotalUnits(unit_amount);
+        Spree.updateQuantity(quantity);
+        Spree.checkStock();
 
-    return radios.click(function(event) {
-      Spree.updateTotalUnits($(this).data('unitAmount'));
-      return console.log(Spree.checkStock());
+        quantity_spinner.addEventListener("input", function(e) {
+          Spree.updateQuantity(quantity_spinner.valueAsNumber);
+          return console.log(Spree.checkStock());
+        })
+
+        return radios.click(function(event) {
+          Spree.updateTotalUnits($(this).data('unitAmount'));
+          return console.log(Spree.checkStock());
+        });
+      }
     });
-  });
-
 }).call(this);
 
